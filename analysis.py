03238c1fea16764331b66f802faffa9a8cda2a1a
Lokesh Thopuri <thopurilokesh@gmail.com>
2025-12-31 15:59:45 +0530

    update

--- analysis.py
+++ analysis.py
@@ -11,11 +11,28 @@ df["OrderDate"]=pd.to_datetime(df["OrderDate"])#it is to convert string date to 
 df["Total_Sales"]=df["Quantity"]*df["Price"]
 #total revenue of the company 
 total_revenue=df["Total_Sales"].sum()
-print(f"Total revenue is :{total_revenue}")
+#print(f"Total revenue is :{total_revenue}")
 #monthly sales trend
 df["Month"]=df["OrderDate"].dt.month
-monthly_analysis=df.groupby("Month")["Total_Sales"].sum()
+monthly_sales=df.groupby("Month")["Total_Sales"].sum()
 #print(f"Monthly Analysis :{monthly_analysis}")
 ### Best selling products
 product_sales=df.groupby("Product")["Quantity"].sum().sort_values(ascending=False)
-print(f"Best selling products:{product_sales}")
+#print(f"Best selling products:{product_sales}")
+#Category_wise Revenue
+category_revenue=df.groupby("Category")["Total_Sales"].sum().sort_values(ascending=False)
+#print(f"Category revenue is:{category_revenue}")
+
+### Customer anaylsis
+top_customers=df.groupby("CustomerID")["Total_Sales"].sum().sort_values(ascending=False)
+print(f"Top Customers are :{top_customers}")
+
+monthly_sales.to_csv("monthly_sales.csv")
+category_revenue.to_csv("category_revenue.csv")
+print("✅ Analysis files saved")
+category_revenue.plot(kind="bar")
+plt.title("Category-wise Revenue")
+plt.xlabel("Category")
+plt.ylabel("Total Revenue")
+plt.show()
+
